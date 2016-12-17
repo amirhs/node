160c3783b948cc3cb311dba70f9dc2c41bee4cb5
Amir <hosseeini.amir@gmail.com>
2016-12-17 22:56:47 +0100

    Moved 'fortune cookie' functionality into module.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
 	var express = require('express');
+	var fortune = require('./lib/fortune.js');
+
 
 	var app = express();
 
@@ -13,13 +15,7 @@
 	app.set('port', process.env.PORT || 3000);
 	app.use(express.static(__dirname + '/public'));
 
-	var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-	];
+	
 
 
 
@@ -33,9 +29,8 @@
 	});
 
 	app.get('/about', function(req, res){
-		var randomFortune =
-			fortunes[Math.floor(Math.random() * fortunes.length)];
-		res.render('about', { fortune: randomFortune});
+
+		res.render('about', { fortune: fortune.getFortune()});
 	});
 
 	// custom 404 page
