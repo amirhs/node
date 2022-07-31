60d53ccc9bedd401f2b55f182eff7354f10b7f99
Amir Hosseini <hosseeini.amir@gmail.com>
2022-07-31 19:37:38 +0200

    Add new Files

--- models/product.js
+++ models/product.js
@@ -2,31 +2,40 @@ const fs = require('fs');
 const path = require('path');
 const pathDir = require('../util/path');
 
-const p = path.join(pathDir, 'data',  'products.json');
+const p = path.join(
+  pathDir,
+  'data',
+  'products.json'
+);
 
 const getProductsFromFile = cb => {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      } else {
-        cb(JSON.parse(fileContent));
-      }
-    });
-  };
-  
-  module.exports = class Product {
-    constructor(t) {
-      this.title = t;
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-  
-    save() {
-      getProductsFromFile(products => {
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {});
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
-    }
-  
-    static fetchAll(cb) {
-      getProductsFromFile(cb);
-    }
-  };
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
